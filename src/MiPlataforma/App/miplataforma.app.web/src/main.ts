eaf7a6196d966a4638c51da1416c7265d51d70cc
Matías Morales <matiasmorales.dev@gmail.com>
2026-08-19 19:15:09 -0300

    Implementación de prime-ui, tailwind y charts en el frontend

--- src/MiPlataforma/App/miplataforma.app.web/src/main.ts
+++ src/MiPlataforma/App/miplataforma.app.web/src/main.ts
@@ -1,6 +1,18 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.mount('#app')
--- src/MiPlataforma/App/miplataforma.app.web/vite.config.ts
+++ src/MiPlataforma/App/miplataforma.app.web/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 61087,
-    }
+  plugins: [vue(), tailwindcss()],
+  server: {
+    port: 61087,
+  },
 })
